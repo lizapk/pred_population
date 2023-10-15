bf1f7c429587e619ddae67343460344a98262c31
lizapk <126601571+lizapk@users.noreply.github.com>
2023-10-16 04:37:34 +0700

    Update global_population.py

--- global_population.py
+++ global_population.py
@@ -1,56 +1,27 @@
-{
-  "nbformat": 4,
-  "nbformat_minor": 0,
-  "metadata": {
-    "colab": {
-      "provenance": [],
-      "authorship_tag": "ABX9TyNQRc4Zgkn55DIyJs/2TZHk"
-    },
-    "kernelspec": {
-      "name": "python3",
-      "display_name": "Python 3"
-    },
-    "language_info": {
-      "name": "python"
-    }
-  },
-  "cells": [
-    {
-      "cell_type": "code",
-      "execution_count": null,
-      "metadata": {
-        "id": "KBr8SIWNQXWi"
-      },
-      "outputs": [],
-      "source": [
-        "import pickle\n",
-        "import streamlit as st\n",
-        "import pandas as pd\n",
-        "import matplotlib.pyplot as plt\n",
-        "\n",
-        "model = pickle.load(open('prediksi_populasi.sav','rb'))\n",
-        "\n",
-        "df = pd.read_csv('Global_annual_population.csv')\n",
-        "df['Year'] = pd.to_datetime(df['Year'], format='%Y')\n",
-        "df.set_index(['Year'], inplace=True)\n",
-        "\n",
-        "st.title('Forecasting Populasi')\n",
-        "year = st.slider(\"Tentukan Tahun\",1,30, step=1)\n",
-        "\n",
-        "pred = model.forecast(year)\n",
-        "pred = pd.DataFrame(pred, columns=['Population'])\n",
-        "\n",
-        "if st.button(\"Predict\"):\n",
-        "\n",
-        "    col1, col2 = st.columns([2,3])\n",
-        "    with col1:\n",
-        "        st.dataframe(pred)\n",
-        "    with col2:\n",
-        "        fig, ax = plt.subplots()\n",
-        "        df['Population'].plot(style='--', color='gray', legend=True, label='known')\n",
-        "        pred['Population'].plot(color='b', legend=True, label='Prediction')\n",
-        "        st.pyplot(fig)"
-      ]
-    }
-  ]
-}
+import pickle
+import streamlit as st
+import pandas as pd
+import matplotlib.pyplot as plt
+
+model = pickle.load(open('prediksi_populasi.sav','rb'))
+
+df = pd.read_csv('Global_annual_population.csv')
+df['Year'] = pd.to_datetime(df['Year'], format='%Y')
+df.set_index(['Year'], inplace=True)
+
+st.title('Forecasting Populasi')
+year = st.slider("Tentukan Tahun",1,30, step=1)
+
+pred = model.forecast(year)
+pred = pd.DataFrame(pred, columns=['Population'])
+
+if st.button("Predict"):
+
+    col1, col2 = st.columns([2,3])
+    with col1:
+        st.dataframe(pred)
+    with col2:
+        fig, ax = plt.subplots()
+        df['Population'].plot(style='--', color='gray', legend=True, label='known')
+        pred['Population'].plot(color='b', legend=True, label='Prediction')
+        st.pyplot(fig)
